f49466bcb27bde42c8db3d528bf77582798a2dde
sulbin <tlsdnr1135@naver.com>
2022-10-14 13:01:08 +0900

    no message

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'real.jar' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가 "
+echo "> $JAR_NAME 에 실행권한 추가  "
 
 chmod +x $JAR_NAME
 
