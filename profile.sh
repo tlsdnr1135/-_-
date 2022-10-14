b03d284c5aa5e1159fb81c25f634358f566de0b9
sulbin <tlsdnr1135@naver.com>
2022-10-15 07:47:01 +0900

    no message

--- profile.sh
+++ profile.sh
@@ -9,14 +9,14 @@ function find_idle_profile()
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-        CURRENT_PROFILE=real2
+        CURRENT_PROFILE=real22
     else
         CURRENT_PROFILE=$(curl -s http://localhost/api/account/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
     then
-      IDLE_PROFILE=real2
+      IDLE_PROFILE=real22
     else
       IDLE_PROFILE=real1
     fi
